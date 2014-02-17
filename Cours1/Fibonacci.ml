11ea88657c8d49c740d1a1cea2699a664822443f
Maigret Aurélien <dewep.net@gmail.com>
2014-02-17 18:03:33 +0100

    Update Fibonacci.ml
    Utilisation des motifs

--- Cours1/Fibonacci.ml
+++ Cours1/Fibonacci.ml
@@ -1,6 +1,8 @@
 let fibonacci n = let rec fibonacci_aux nm2 nm1 na nm =
-  if nm < 0 then failwith("Invalid number")
-  else if nm == 0 then 0
-  else if na >= nm then nm2 + nm1
-  else fibonacci_aux nm1 (nm1 + nm2) (na + 1) nm
+  match na with
+    | x when nm < 0 -> failwith("Invalid number")
+    | x when nm = 0 -> 0
+    | x when nm = 1 -> 1
+    | x when x = nm -> nm2 + nm1
+    | _ -> fibonacci_aux nm1 (nm1 + nm2) (na + 1) nm
   in fibonacci_aux 0 1 2 n

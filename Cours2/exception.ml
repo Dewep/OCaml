33b40fd206be2db3c5e79fc791c9ff4dcf49ff45
Maigret Aurélien <dewep.net@gmail.com>
2014-02-24 19:03:16 +0100

    Create exception.ml

---
+++ Cours2/exception.ml
@@ -0,0 +1,11 @@
+let list_mul l =
+  let rec list_mul_impl = function
+    | [] -> 1
+    | h::t -> if h <> 0 then h * list_mul_impl t
+      else raise Invalid_argument("0 found")
+  in
+  try
+    list_mul_impl l
+  with
+    | Invalid_argument(s) -> begin print_string s; 0 end
+    | e -> raise e

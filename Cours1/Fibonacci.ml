eff7d405464e618bc958e5a0fbb26e19042f9cb6
Maigret Aurélien <dewep.net@gmail.com>
2014-02-25 14:31:12 +0100

    Update Fibonacci.ml
    Coucou Valentin !

--- Cours1/Fibonacci.ml
+++ Cours1/Fibonacci.ml
@@ -1,8 +1,15 @@
+let fibonacci n = let rec fibonacci_aux x y z =
+    if z <= 0 then 0
+    else if z <= 1 then y
+    else fibonacci_aux (y) (x + y) (z - 1)
+  in fibonacci_aux 0 1 n
+
+
 let fibonacci n = let rec fibonacci_aux nm2 nm1 na nm =
   match na with
-    | x when nm < 0 -> failwith("Invalid number")
-    | x when nm = 0 -> 0
-    | x when nm = 1 -> 1
-    | x when x = nm -> nm2 + nm1
+    | _ when nm < 0 -> failwith("Invalid number")
+    | 0 -> 0
+    | 1 -> 1
+    | nm -> nm2 + nm1
     | _ -> fibonacci_aux nm1 (nm1 + nm2) (na + 1) nm
   in fibonacci_aux 0 1 2 n

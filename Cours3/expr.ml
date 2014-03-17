4f7989dc25b32d95cc38aeca95b98b96368b69dd
Maigret Aurélien <dewep.net@gmail.com>
2014-03-17 17:37:14 +0100

    Update expr.ml

--- Cours3/expr.ml
+++ Cours3/expr.ml
@@ -6,6 +6,11 @@ type expression =
     | Mod of expression * expression
     | Val of int
 
+let upu = Sum(Val(1), Val(1))
+let upupu = Sum(Val(1), Sum(Val(1), Val(1)))
+(* 1+2x3 *) let updft = Sum(Val(1), Mul(Val(2), Val(3)))
+(* (1-2)x3/4 *) let tototo = Div(Mul(Sub(Val(1), Val(2)), Val(3)), Val(4))
+
 (*
         (1+2)*3:
                 *
